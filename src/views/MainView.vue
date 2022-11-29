4f650059064ee814b7fa0853072bed2963955700
JuKyYoon <yoongh97@gmail.com>
2022-11-29 11:02:11 +0900

    fix: fix a modal close button

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -81,7 +81,7 @@
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                  aria-hidden="true">times</span>
+                  aria-hidden="true" v-on:click="closeCreateChatroomModal">&times;</span>
               </button>
               <h4 class="modal-title" id="myModalLabel">Create QueryRoom</h4>
             </div>
